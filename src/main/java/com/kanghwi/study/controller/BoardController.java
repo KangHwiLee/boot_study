a0387153b82a9308f7d725287d8cd104a3150515
kanghwi <rkdgnl12336@gmail.com>
2023-02-14 22:39:32 +0900

    게시판 글쓰기 및 validator를 이용한 유효성검사

--- src/main/java/com/kanghwi/study/controller/BoardController.java
+++ src/main/java/com/kanghwi/study/controller/BoardController.java
@@ -2,12 +2,14 @@ package com.kanghwi.study.controller;
 
 import com.kanghwi.study.model.Board;
 import com.kanghwi.study.repository.BoardRepository;
+import com.kanghwi.study.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -22,5 +24,29 @@ public class BoardController {
         List<Board> boards = boardRepository.findAll();
         model.addAttribute("boards", boards);
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id){
+        System.out.println(id);
+        if(id == null){
+            model.addAttribute("board", new Board());
+        }
+        else model.addAttribute("board", boardRepository.findById(id).orElse(null));
+
+        return "board/form";
+    }
+
+    @Autowired
+    private BoardValidator boardValidator;
+
+    @PostMapping("/form")
+    public String form(@Valid Board board, BindingResult bindingResult){
+        boardValidator.validate(board, bindingResult);
+        if(bindingResult.hasErrors()){
+            return "board/form";
+        }
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
--- src/main/java/com/kanghwi/study/model/Board.java
+++ src/main/java/com/kanghwi/study/model/Board.java
@@ -1,18 +1,24 @@
 package com.kanghwi.study.model;
 
-import lombok.Data;
+import com.sun.istack.NotNull;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
 @Entity
-@Data
+@Getter
+@Setter
+@ToString
 public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotNull
+    @Size(min = 2,max = 30)
     private String title;
     private String content;
 }
